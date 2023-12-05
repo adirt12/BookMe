12dd9780a7c713ca63e5ffb436ab12c7199d6867
adirt12 <adirt12@gmail.com>
2023-12-05 09:49:04 +0200

    signup password fixed

--- app/(home)/signUpForm.js
+++ app/(home)/signUpForm.js
@@ -1,7 +1,7 @@
 import { Alert, Pressable, ScrollView, StyleSheet, Text, View, KeyboardAvoidingView, StatusBar } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { LinearGradient } from "expo-linear-gradient";
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { Ionicons, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { TextInput } from 'react-native-gesture-handler';
 import { useRouter } from "expo-router";
 import axios from "axios";
@@ -10,7 +10,7 @@ import PhoneInput from 'react-phone-input-2'
 import * as Network from 'expo-network';
 import Constants from 'expo-constants';
 
-// import 'react-phone-input-2/lib/style.css'
+
 
 const signUpForm = () => {
   const router = useRouter();
@@ -21,13 +21,15 @@ const signUpForm = () => {
   const [password, setpassword] = useState("");
   const [dateOfBirth, setdateOfBirth] = useState(new Date());
   const [phoneNumber, setphoneNumber] = useState("");
-  // const [phoneNumber,setphoneNumber]=useState("");
-
+  const [showPassword, setShowPassword] = useState(false);
   const [showDatePicker, setShowDatePicker] = useState(false);
-  // const [date, setdateOfBirth] = useState(new Date());
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   const onChange = (e, selectedDate) => {
-    // setdateOfBirth(selectedDate);
-    setShowDatePicker(false); // Close the picker on iOS
+    setShowDatePicker(false);
     if (selectedDate) {
       setdateOfBirth(selectedDate);
     }
@@ -44,6 +46,15 @@ const signUpForm = () => {
   };
 
   const handleReg = async () => {
+
+    if (password.length < 6) {
+      Alert.alert(
+        'Invalid Password',
+        'Password must be at least 6 characters long.'
+      );
+      return;
+    }
+    
     const ipAddress = Constants.expoConfig.hostUri.split(':')[0];
     const userData = {
       email: email,
@@ -108,47 +119,52 @@ const signUpForm = () => {
             placeholder='Please enter LastName' style={{ width: '90%', height: 50, margin: 12, borderWidth: 2, borderRadius: 10, }} />
 
           <Text style={{ margin: 5 }}>Password</Text>
-          <TextInput value={password} onChangeText={(text) => setpassword(text)}
-            placeholder='Please enter Password' style={{ width: '90%', height: 50, margin: 12, borderWidth: 2, borderRadius: 10, }} />
+          <View style={{flexDirection: 'row', width: '90%', height: 50, margin: 12, borderWidth: 2, borderRadius: 10,alignItems: 'center',justifyContent: 'center',}}>
+            <TextInput value={password} onChangeText={(text) => setpassword(text)}
+              placeholder='Please enter Password'  secureTextEntry={!showPassword} style={{flex:1}}/>
+            <MaterialCommunityIcons
+              name={showPassword ? 'eye-off' : 'eye'}
+              size={24}
+              color="#aaa"
+              style={{marginRight:15}}
+              onPress={toggleShowPassword}
+            />
+          </View>
 
           <Text style={{ margin: 5 }}>Date Of Birth</Text>
-          {/* <TextInput value={dateOfBirth} onChangeText={(text) => setdateOfBirth(text)}
-          placeholder='Please enter Date Of Birth' style={{ width: '90%', height: 50, margin: 12, borderWidth: 2, borderRadius: 10, }} /> */}
           <View style={{ alignItems: 'center', flex: 1 }}>
-          {Platform.OS === 'android' &&(
-              <Pressable onPress={() => setShowDatePicker(true)} style={{ width: '50%', height: 50,  borderColor: 'black',borderWidth: 2,borderRadius:30,alignItems:'center',justifyContent:'center'}} >
-              <Text style={{fontSize:25}}>{dateOfBirth.toLocaleDateString()}</Text>
+            {Platform.OS === 'android' && (
+              <Pressable onPress={() => setShowDatePicker(true)} style={{ width: '50%', height: 50, borderColor: 'black', borderWidth: 2, borderRadius: 30, alignItems: 'center', justifyContent: 'center' }} >
+                <Text style={{ fontSize: 25 }}>{dateOfBirth.toLocaleDateString()}</Text>
               </Pressable>
-              )}
-                {Platform.OS === 'android' && showDatePicker && (
-                  <DateTimePicker
-                    value={dateOfBirth}
-                    mode={"date"}
-                    is24Hour={true}
-                    onChange={onChange}
-                  />
-                )}
-              
-          
-            {Platform.OS === 'ios'&&(
-                  <DateTimePicker
-                    value={dateOfBirth}
-                    mode={"date"}
-                    is24Hour={true}
-                    onChange={onChange}
-                  />
-                )}
+            )}
+            {Platform.OS === 'android' && showDatePicker && (
+              <DateTimePicker
+                value={dateOfBirth}
+                mode={"date"}
+                is24Hour={true}
+                onChange={onChange}
+              />
+            )}
+
+
+            {Platform.OS === 'ios' && (
+              <DateTimePicker
+                value={dateOfBirth}
+                mode={"date"}
+                is24Hour={true}
+                onChange={onChange}
+              />
+            )}
           </View>
 
           <Text style={{ margin: 5 }}>Phone Number</Text>
-          {/* <TextInput value={phoneNumber} onChangeText={(text) => setphoneNumber(text)}
-            placeholder='Please enter Phone Number' style={{ width: '90%', height: 50, margin: 12, borderWidth: 2, borderRadius: 10, }} /> */}
           <TextInput
             placeholder="Enter phone number"
             value={phoneNumber}
             onChangeText={handlePhoneNumberChange}
             keyboardType="numeric"
-            maxLength={10} // Limit the input to 12 characters
+            maxLength={10}
             style={{ width: '90%', height: 50, margin: 12, borderWidth: 2, borderRadius: 10, }}
           />
         </View>
